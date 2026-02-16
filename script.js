cc57df5bbecd63a6590ebde6d907ce4ddff15163
adarsh720-ab <bhosaleadarsh720@gmail.com>
2026-02-16 13:55:12 +0530

    Toggle button for mobile view issue solved

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+
+document.querySelectorAll('.navbar-collapse .nav-link, .navbar-collapse .btn')
+  .forEach(function(item) {
+    item.addEventListener('click', function() {
+      let navbarCollapse = document.querySelector('.navbar-collapse');
+      let bsCollapse = new bootstrap.Collapse(navbarCollapse, {
+        toggle: false
+      });
+      bsCollapse.hide();
+    });
+  });
+
+
 const imageModal = document.getElementById("imageModal");
 
 imageModal.addEventListener("show.bs.modal", function (event) {
